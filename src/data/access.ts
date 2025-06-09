de30f53771de713e275b81947696a660c5f8330a
Mateusz Zalewski <mateusz.zalewski@sabre.com>
2025-06-09 16:35:32 +0200

    Replace isPublic and isAdmin with enums.

--- src/data/access.ts
+++ src/data/access.ts
@@ -1,16 +1,16 @@
-import { User } from "@prisma/client";
+import { BlogVisibility, Role, User } from "@prisma/client";
 import { BlogDTO } from "./blog-dto";
 
 export function canUserSeeBlog(user: User | null, blog: BlogDTO): boolean {
-  if (blog.isPublic) return true;
-  if (user?.isAdmin) return true;
+  if (blog.visibility === BlogVisibility.PUBLIC) return true;
+  if (user?.role === Role.ADMIN) return true;
   if (user && user.id === blog.author?.id) return true;
   return false;
 }
 
 export function canUserEditBlog(user: User | null, blog: BlogDTO): boolean {
   if (!user) return false;
-  if (user.isAdmin) return true;
+  if (user.role === Role.ADMIN) return true;
   if (blog.author && blog.author.id === user.id) return true;
   return false;
 }

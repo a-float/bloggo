95cbfe19c130ad462d9693a92b4d6d54f95b1720
Mateusz Zalewski <mati852456@gmail.com>
2025-07-01 00:08:55 +0200

    Fix footer not sticking to the bottom

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,9 +1,8 @@
-
 export default function NotFound() {
   return (
-    <div className="flex flex-col items-center justify-center pt-32 max-w-md mx-auto">
+    <div className="flex flex-col items-center justify-center pt-32 max-w-md mx-auto text-center">
       <h1 className="text-4xl font-bold mb-4">Page Not Found</h1>
-      <p className="text-md mb-8 text-center">
+      <p className="text-md mb-8">
         <a href="/" className="link">
           Go back home
         </a>

92d7d51715b492aa323958f350236bce567282e7
Mateusz Zalewski <mateusz.zalewski@sabre.com>
2025-06-30 22:33:53 +0200

    Fix friend can't view blog

--- src/data/access.ts
+++ src/data/access.ts
@@ -1,10 +1,22 @@
 import { BlogVisibility, Role, User } from "@prisma/client";
 import { BlogDTO } from "./blog-dto";
+import * as friendService from "@/lib/service/friend.service";
 
-export function canUserSeeBlog(user: User | null, blog: BlogDTO): boolean {
+export async function canUserSeeBlog(
+  user: User | null,
+  blog: BlogDTO
+): Promise<boolean> {
   if (blog.visibility === BlogVisibility.PUBLIC) return true;
-  if (user?.role === Role.ADMIN) return true;
-  if (user && user.id === blog.author?.id) return true;
+  if (!user) return false;
+  if (user.role === Role.ADMIN) return true;
+  if (!blog.author) return false;
+  if (user.id === blog.author.id) return true;
+  if (
+    blog.visibility === BlogVisibility.FRIENDS &&
+    (await friendService.areUsersFriends(user, blog.author))
+  ) {
+    return true;
+  }
   return false;
 }
 

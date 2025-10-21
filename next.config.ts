093018b891cb5cf66905b79041114eff2beaf443
Mateusz Zalewski <mati852456@gmail.com>
2025-10-21 10:48:56 +0200

    Configure stateTime on dynamic pages

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   experimental: {
     authInterrupts: true,
+    staleTimes: {
+      dynamic: 60,
+    },
   },
   poweredByHeader: false,
   images: {

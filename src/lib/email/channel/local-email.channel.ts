a842ff9333ba6c03b3f8506422e8000114e4dd58
Mateusz Zalewski <mati852456@gmail.com>
2026-07-12 00:06:31 +0200

    Switch from mailhog to mailpit

--- src/lib/email/channel/local-email.channel.ts
+++ src/lib/email/channel/local-email.channel.ts
@@ -9,10 +9,10 @@ export class LocalEmailChannel extends EmailChannel {
     this.transport = createTransport({
       host: process.env.EMAIL_SERVER_HOST,
       port: parseInt(process.env.EMAIL_SERVER_PORT!),
-      auth: {
-        user: process.env.EMAIL_SERVER_USER,
-        pass: process.env.AUTH_RESEND_KEY,
-      },
+      // auth: {
+      //   user: process.env.EMAIL_SERVER_USER,
+      //   pass: process.env.AUTH_RESEND_KEY,
+      // },
     });
     return this;
   }
